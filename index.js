8952774dfd7edeacd374a40e5767dd651b15b224
maxim <godofmadness>
2017-10-09 21:48:09 +0300

    enc/dec using encode table

--- index.js
+++ index.js
@@ -2,69 +2,160 @@ var $text = document.getElementById("notEncodedText");
 var $result = document.getElementById("result");
 var $keyInput = document.getElementById("keyInput");
 var $buttonEnc = document.getElementById("buttonEnc");
+var $buttonDec = document.getElementById("buttonDec");
 
 var aplhabet = "abcdefghijklmnopqrstuvwxyz";
 var encoded = "";
 // button click
 
-function findIndexInAlphabeth(letter) {
-	for (var i = 0; i < aplhabet.length; i++) {
-		if (aplhabet[i]	===  letter) {
-			return i;
-		}
-	}
+// function findIndexInAlphabeth(letter) {
+// 	for (var i = 0; i < aplhabet.length; i++) {
+// 		if (aplhabet[i]	===  letter) {
+// 			return i;
+// 		}
+// 	}
+// }
+
+
+// var encodeTable = {
+// 	"a": "a",
+// 	"b": "a",
+// 	"c": "a",
+// 	"d": "a",
+// 	"e": "a",
+// 	"f": "a",
+// 	"g": "a",
+// 	"h": "a",
+// 	"i": "a",
+// 	"j": "a",
+// 	"k": "a",
+// 	"l": "a",
+// 	"m": "a",
+// 	"n": "a",
+// 	"o": "a",
+// 	"p": "a",
+// 	"q": "a",
+// 	"r": "a",
+// 	"s": "a",
+// 	"t": "a",
+// 	"u": "a",
+// 	"v": "a",
+// 	"w": "a",
+// 	"y": "a",
+// 	"z": "a"
+// }
+
+
+var i = 0;
+
+function mapIndex(index, key) {
+	return ((index + 1) * key -1 ) % aplhabet.length;
 }
 
 
-function calculateOffset(index) {
-	return ((index + 1) * $keyInput.value) % aplhabet.length
-}
-
-
-
-
+// TODO comibne to one listener
 
 $buttonEnc.addEventListener("click", function(){
-	$result.textContent = ""
+	
+	var table = createEncodeTableFromKey($keyInput.value);
 
+	$result.textContent = encode(table, $text.value);
+});
 
-	// to encode text value
-	var text = $text.value;
+$buttonDec.addEventListener("click", function(){
+	
+	// if ()
+	// TODO implement key validation
+	var table = createEncodeTableFromKey($keyInput.value);
 	
 
-
-	for (var i = 0; i < text.length; i++) {
-
-		var alphabetIndex = findIndexInAlphabeth(text[i]);
-
-		console.log(alphabetIndex);
-		var offset = calculateOffset(alphabetIndex);
-		var indexOfEncodedLetter = (offset + alphabetIndex) % 26;
-
-		// console.log(indexOfEncodedLetter)
-		// console.log(aplhabet[indexOfEncodedLetter])
-
-		encoded += aplhabet[indexOfEncodedLetter];
-
-	}
-
-
-
-
-
-	// result out on screen 
-	var textNode = document.createTextNode(encoded);
-	$result.appendChild(textNode);
-
-
+	$result.textContent = decode(table, $text.value);
 });
 
 
 
+function encode(encodeTable, text) {
+
+	var resulr = "";
+
+	for (var i = 0; i < text.length; i++) {
+		resulr += encodeTable[text[i]];
+	}
+
+	return resulr;
+}
+
+function decode(encodeTable, text) {
+	
+		var resulr = "";
+
+		// TODO finish decoder
+		for (var i = 0; i < text.length; i++) {
+			resulr += 
+		}
+	
+		return resulr;
+	}
+
+
+$buttonDec.addEventListener("click", function(){
 
 
 
-console.log("branch test 1")
+	
+
+
+// 	// if mapped letter is in alphabeth -> 
+// 	//decode
+
+
+
+// 	// if not ->
+// 	// create new aplhabeth from key
+
+
+function createEncodeTableFromKey(key) {
+
+	var table = {};
+
+	for (var i = 0; i < aplhabet.length; i++) {
+		table[aplhabet[i]] = aplhabet[mapIndex(i, key)];
+	}
+
+	return table;
+
+}
+
+// $buttonEnc.addEventListener("click", function(){
+// 	$result.textContent = ""
+
+
+// 	// to encode text value
+// 	var text = $text.value;
+
+// 	for (var i = 0; i < text.length; i++) {
+
+// 		var alphabetIndex = findIndexInAlphabeth(text[i]);
+// 		var newAlphabethIndex = (alphabetIndex * $keyInput.value) % 26;
+// 		console.log(newAlphabethIndex);
+// 		encoded += aplhabet[newAlphabethIndex];
+
+// 	}
+
+
+// 	// result out on screen 
+// 	var textNode = document.createTextNode(encoded);
+// 	$result.appendChild(textNode);
+
+
+// });
+
+
+
+
+
+
+// console.log("branch test 1")
 
 
 
