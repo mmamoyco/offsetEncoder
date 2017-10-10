440fa6a5bdeb1ee615f1e20a9b4781ae5fd186d7
Alexander Kiselev <kiselev20060@gmail.com>
2017-10-10 18:14:06 +0300

    refactored; checkers added

--- index.js
+++ index.js
@@ -1,64 +1,63 @@
 var $notEncodedText = document.getElementById("not-encoded-text");
 var $encodedText = document.getElementById("encoded-text");
 var $keyInput = document.getElementById("key-input");
-var $errorMessage = document.getElementById("error-message");
 var $buttonEnc = document.getElementById("button-enc");
 var $buttonDec = document.getElementById("button-dec");
 var $buttonContainer = document.getElementsByClassName("button-container")[0];
 
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-// т$keyInput.value = 3;
-
-// $encodedText.value = "HWEEQWE";
-
 function mapIndex(index, key) {
 	return ((index + 1) * key -1 ) % alphabet.length;
 }
 
-// TODO: Checking relatibely prime numbers
-
-$buttonContainer.addEventListener("click", function(e) {
+$buttonContainer.addEventListener("click", function encryption(e) {
 	var table;
 
-	if (e.target === $buttonEnc) {
-		$errorMessage.textContent = "";
-		check = checker($keyInput.value, alphabet);
+	check = checker($keyInput.value, alphabet);
 
-		if (check == -1) {
-			$errorMessage.textContent = "Key < 0";
-		} else if (check == 0) {
-			table = createEncodeTableFromKey($keyInput.value);
-			$encodedText.value = encode(table, $notEncodedText.value);
-		} else {
+	switch (check) {
+		case -1:
+			toastr.error("Key < 0");
+			break;
+		case 13:
+		case 2:
 			toastr.error("Error in " + check + " char");
-			// $errorMessage.textContent = "Error in " + check + " char";
-			// TODO refactoring of duplication + use toast.error(text) to display error message
-		}
-	} else if (e.target === $buttonDec) {
-		$errorMessage.textContent = "";
-		check = checker($keyInput.value, alphabet);
-
-		if (check == -1) {
-			$errorMessage.textContent = "Key < 0";
-		} else if (check == 0) {
-			table = createDecodeTableFromKey($keyInput.value);
-			$notEncodedText.value = encode(table, $encodedText.value);
-		} else {
-			toastr.error("Error in " + check + " char");
-			// $errorMessage.textContent = "Error in " + check + " char";
-		}
+			break;
+		case -2:
+			toastr.error("Key is undefined");
+			break;
+		default:
+			if (e.target === $buttonEnc) {
+				table = createEncodeTableFromKey($keyInput.value);
+				$encodedText.value = encode(table, $notEncodedText.value);
+				$notEncodedText.value = "";
+			} else {
+				table = createDecodeTableFromKey($keyInput.value);
+				$notEncodedText.value = encode(table, $encodedText.value);
+				$encodedText.value = "";
+			}
 	}
 });
-
 
 function encode(encodeTable, text) {
 	var result = "";
 
 	for (var i = 0; i < text.length; i++) {
+		if (encodeTable[text[i]] != undefined) {
 			result += encodeTable[text[i]];
+		} else {
+			toastr.error("Source string contains invalid characters");
+			return "";
+		}
 	}
-	return result;
+
+	if (result == "") {
+		toastr.error("Source string is undefined");
+	} else {
+		toastr.success("Operation successful!");
+		return result;
+	}
 }
 
 
@@ -69,12 +68,7 @@ function createEncodeTableFromKey(key) {
 		table[alphabet[i]] = alphabet[mapIndex(i, key)];
 	}
 
-	table[" "] = " ";
-
-	for (i = 0; i < 10; i++) {
-		table[i] = i;
-	}
-
+	commonTablesCompletion(table);
 	return table;
 }
 
@@ -86,10 +80,20 @@ function createDecodeTableFromKey(key) {
 		table[alphabet[mapIndex(i, key)]] = alphabet[i];
 	}
 
+	commonTablesCompletion(table);
 	return table;
 }
 
-function nod(firstNumber, secondNumber) {
+// add numbers and space to table
+function commonTablesCompletion(table) {
+	table[" "] = " ";
+
+	for (i = 0; i < 10; i++) {
+		table[i] = i;
+	}
+}
+
+function greatestCommonDivisor(firstNumber, secondNumber) {
 	while (firstNumber != 0 && secondNumber != 0) {
 		if (firstNumber > secondNumber) {
 			firstNumber %= secondNumber;
@@ -97,33 +101,26 @@ function nod(firstNumber, secondNumber) {
 			secondNumber %= firstNumber;
 		}
 	}
-	if (firstNumber == 0) {
-		return secondNumber;
-	} else {
-		return firstNumber;
-	}
+
+	var result = (firstNumber == 0) ? secondNumber : firstNumber;
+
+	return result;
 }
 
 function isRelativelyPrimeNumbers(firstNumber, secondNumber) {
-	if (nod(firstNumber, secondNumber) != 1) {
-		return false;
-	} else {
-		return true;
-	}
+	return (greatestCommonDivisor(firstNumber, secondNumber) != 1) ? false : true;
 }
 
 function checker(key, alphabeth) {
+	var check = 0;
+
 	if (key < 0) {
-		return -1;
+		check = -1;
+	} else if (key == "") {
+		check = -2;
+	} else if (!isRelativelyPrimeNumbers(key, alphabeth.length)) {
+		check = (greatestCommonDivisor(key, alphabeth.length) == 2) ? 13 : 2;
 	}
 
-	if (!isRelativelyPrimeNumbers(key, alphabeth.length)) {
-		if (nod(key, alphabeth.length) == 2) {
-			return 13;
-		} else {
-			return 2;
-		}
-	} else {
-		return 0;
-	}
+	return check;
 }
